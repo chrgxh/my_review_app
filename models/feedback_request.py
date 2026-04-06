1e269b121db28bdcb16911cdab994a5724296644
Christos Gkatzis <christos.a.gkatzis@gmail.com>
2026-04-06 10:20:38 +0300

    Add indexes in feedback requests

--- models/feedback_request.py
+++ models/feedback_request.py
@@ -1,16 +1,21 @@
-from datetime import datetime,UTC
+from datetime import datetime, UTC
 from typing import Optional
 
+from sqlalchemy import Index
 from sqlmodel import SQLModel, Field
+
 
 class FeedbackRequest(SQLModel, table=True):
     __tablename__ = "feedback_requests"
+    __table_args__ = (
+        Index("ix_feedback_requests_business_sent_at", "business_id", "sent_at"),
+    )
 
     id: Optional[int] = Field(default=None, primary_key=True)
     business_id: int = Field(foreign_key="businesses.id", index=True)
     sent_by_user_id: int = Field(foreign_key="business_users.id", index=True)
     recipient_email: str = Field(index=True)
-    identifier: str
+    identifier: str = Field(index=True)
     message: Optional[str] = None
     token: str = Field(index=True, unique=True)
     email_provider_id: Optional[str] = Field(default=None, index=True)
